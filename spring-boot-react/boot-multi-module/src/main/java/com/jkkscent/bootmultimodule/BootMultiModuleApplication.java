94d5ab7f9b7e816bb18f5caa31194e1935469f35
kopro-ov <koprodev@gmail.com>
2021-03-15 13:07:20 +0900

    구조 변경

---
+++ spring-boot-react/boot-multi-module/src/main/java/com/jkkscent/bootmultimodule/BootMultiModuleApplication.java
@@ -0,0 +1,13 @@
+package com.jkkscent.bootmultimodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootMultiModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootMultiModuleApplication.class, args);
+	}
+
+}
