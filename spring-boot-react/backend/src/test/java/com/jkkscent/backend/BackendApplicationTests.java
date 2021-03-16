732698ad598ac16c5c6d878c79762684d5a97df6
kopro-ov <koprodev@gmail.com>
2021-03-16 09:21:44 +0900

    reset

--- spring-boot-react/backend/src/test/java/com/jkkscent/backend/BackendApplicationTests.java
+++ spring-boot-react/backend/src/test/java/com/jkkscent/backend/BackendApplicationTests.java
@@ -1,10 +1,10 @@
-package com.jkkscent.bootmultimodule;
+package com.jkkscent.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BootMultiModuleApplicationTests {
+class BackendApplicationTests {
 
 	@Test
 	void contextLoads() {
