2a4d07a7a6ee9aa88a6b49890f7a4c27cc8cdfd2
kopro-ov <koprodev@gmail.com>
2021-03-16 17:53:14 +0900

    yarn axios add Scents

---
+++ spring-boot-react/user-frontend/src/Scents.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+//https://react.vlpt.us/integrate-api/01-basic.html
+function Scents() {
+  const [scents, setScents] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchScents = async () => {
+      try {
+        // 요청이 시작 할 때에는 error 와 users 를 초기화하고
+        setError(null);
+        setScents(null);
+        // loading 상태를 true 로 바꿉니다.
+        setLoading(true);
+        const response = await axios.get(
+          'http://192.168.0.167:1202/scent'
+        );
+        setScents(response.data); // 데이터는 response.data 안에 들어있습니다.
+      } catch (e) {
+        console.log(e);
+        setError(e);
+      }
+      setLoading(false);
+    };
+
+    fetchScents();
+  }, []);
+
+  if (loading) return <div>로딩중..</div>;
+  if (error) return <div>에러가 발생했습니다</div>;
+  if (!scents) return null;
+  return (
+    <ul>
+      {scents.map(scent => (
+        <li key={scent.id}>
+          {scent.name} ({scent.name})
+        </li>
+      ))}
+    </ul>
+  );
+
+}
+
+export default Scents;
